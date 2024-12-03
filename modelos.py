d5ad26a85b3fe42598c30a8e650dd084dbff57c5
PedroBartsch <pedro_bartsch-silva@estudante.sesisenai.org.br>
2024-12-03 17:28:25 -0300

    Correção final do modelo

--- modelos.py
+++ modelos.py
@@ -1,12 +1,13 @@
 class Produto:
-    def __init__(self,nome, codigo, preco, quantidade):
+    def __init__(self, nome, codigo, preco, quantidade, tipo):
         self.nome = nome
         self.codigo = codigo
         self.preco = preco
         self.quantidade = quantidade
+        self.tipo = tipo  # Novo campo para tipo de medicamento
 
     def __str__(self):
-        return f'{self.nome} - Preço: R${self.preco} - Estoque: {self.quantidade}'
+        return f"{self.nome} ({self.tipo}) - R${self.preco} - Quantidade: {self.quantidade} - Tipo: {self.tipo}"
     
     def atualizar_estoque(self,quantidade_vendida):
         """Função para atualizar a quantidade de produto após uma venda"""
@@ -18,5 +19,4 @@ class Produto:
     def salvar(self, estoque):
         """Salva ou atualiza o produto no estoque"""
         estoque[self.codigo] = self
-
     
